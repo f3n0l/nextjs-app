cfb13ee963d8e147c1e1b1076f608372f814c479
Tolga Simsek <tol_simsek@live.de>
2023-11-02 20:45:43 +0100

    simple counter

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Counter from "../components/counter";
+
+const CounterPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Counter Page</h1>
+            <Counter />
+        </div>
+    );
+};
+
+export default CounterPage;
